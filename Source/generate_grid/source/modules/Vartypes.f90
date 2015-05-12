cabbcfcc0758501152906382765149d3c4a843ba
Lukasz Mentel <lmmentel@gmail.com>
2015-05-12 18:45:25 +0200

    merge with the local version on lisa

--- Source/generate_grid/source/modules/Vartypes.f90
+++ Source/generate_grid/source/modules/Vartypes.f90
@@ -7,8 +7,8 @@ module Vartypes
    public :: KINT, KREAL, DP, LCHARS
 
    integer, parameter :: KINT  = 8
-   integer, parameter :: KREAL = 8
    integer, parameter :: DP = 8
+   integer, parameter :: KREAL = 8
    integer, parameter :: LCHARS = 128
 
 end module
